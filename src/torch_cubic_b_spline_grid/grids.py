add grid optimisation tests and fix some numerical instability issues

src/torch_cubic_b_spline_grid/grids.py
@@ -65,9 +65,10 @@ class CubicBSplineGrid(torch.nn.Module):
             return u
         else:
             u = torch.atleast_2d(u)
-            if ndim := u.shape[-1] != self.ndim:
+            if u.shape[-1] != self.ndim:
+                ndim = u.shape[-1]
                 raise ValueError(
-                    f'Cannot interpolate {self.ndim} grid with {ndim} coordinates'
+                    f'Cannot interpolate {self.ndim}D grid with {ndim}D coordinates'
                 )
         return u
 

src/torch_cubic_b_spline_grid/interpolate_grid.py
@@ -35,8 +35,7 @@ def interpolate_grid_1d(grid: torch.Tensor, u: torch.Tensor):
     """
     if grid.ndim == 1:
         grid = einops.rearrange(grid, 'w -> 1 w')
-    epsilon = 1e-6
-    u[u == 1] -= epsilon
+    u = torch.clamp(u, min=0, max=1)
     n_samples = grid.shape[-1]
 
     # handle interpolation at edges by extending grid of control points according to
@@ -80,6 +79,7 @@ def interpolate_grid_2d(grid: torch.Tensor, u: torch.Tensor):
     if grid.ndim == 2:
         grid = einops.rearrange(grid, 'h w -> 1 h w')
     _, n_samples_h, n_samples_w = grid.shape
+    u = torch.clamp(u, min=0, max=1)
 
     # pad grid to handle interpolation at edges.
     grid = pad_grid_2d(grid)
@@ -135,6 +135,7 @@ def interpolate_grid_3d(grid, u):
     if grid.ndim == 3:
         grid = einops.rearrange(grid, 'd h w -> 1 d h w')
     _, n_samples_d, n_samples_h, n_samples_w = grid.shape
+    u = torch.clamp(u, min=0, max=1)
 
     # expand grid to handle interpolation at edges
     grid = pad_grid_3d(grid)
@@ -200,6 +201,7 @@ def interpolate_grid_4d(grid: torch.Tensor, u: torch.Tensor):
     if grid.ndim == 4:
         grid = einops.rearrange(grid, 't d h w -> 1 t d h w')
     _, n_samples_t, n_samples_d, n_samples_h, n_samples_w = grid.shape
+    u = torch.clamp(u, min=0, max=1)
 
     # expand grid to handle interpolation at edges
     grid = pad_grid_4d(grid)

tests/test_grid_optimisation.py
@@ -1,12 +1,14 @@
+import einops
 import torch
 
-from torch_cubic_b_spline_grid import CubicBSplineGrid1d
+from torch_cubic_b_spline_grid import CubicBSplineGrid1d, CubicBSplineGrid2d, \
+    CubicBSplineGrid3d
 
 
 def test_1d_grid_optimisation():
-    n_control_points = 6
+    grid_resolution = 6
     n_observations_per_iteration = 20
-    grid = CubicBSplineGrid1d(resolution=n_control_points, n_channels=1)
+    grid = CubicBSplineGrid1d(resolution=grid_resolution, n_channels=1)
 
     def f(x: torch.Tensor, add_noise: bool = False):
         y = torch.sin(x * 2 * torch.pi)
@@ -14,8 +16,8 @@ def test_1d_grid_optimisation():
             y += torch.normal(mean=torch.zeros(len(y)), std=0.3)
         return y
 
-    optimiser = torch.optim.SGD(lr=0.02, params=grid.parameters())
-    for i in range(5000):
+    optimiser = torch.optim.SGD(lr=0.1, params=grid.parameters())
+    for i in range(1000):
         x = torch.rand(size=(n_observations_per_iteration, ))
         observations = f(x, add_noise=True)
         prediction = grid(x).squeeze()
@@ -25,6 +27,68 @@ def test_1d_grid_optimisation():
         optimiser.zero_grad()
 
     x = torch.linspace(0, 1, steps=100)
+    ground_truth = f(x)
+    prediction = grid(x).squeeze()
+    mean_absolute_error = torch.mean(torch.abs(prediction - ground_truth))
+    assert mean_absolute_error.item() < 0.02
+
+
+def test_2d_grid_optimisation():
+    grid_resolution = (6, 6)
+    n_observations_per_iteration = 1000
+    grid = CubicBSplineGrid2d(resolution=grid_resolution, n_channels=1)
+
+    def f(x: torch.Tensor, add_noise: bool = False):
+        centered = x - 0.5
+        y = torch.sqrt(torch.sum(centered**2, dim=-1))  # (x**2 + y**2) ** 0.5
+        if add_noise is True:
+            y += torch.normal(mean=torch.zeros(len(y)), std=0.3)
+        return y
+
+    optimiser = torch.optim.SGD(lr=0.1, params=grid.parameters())
+    for i in range(1000):
+        x = torch.rand(size=(n_observations_per_iteration, 2))
+        observations = f(x, add_noise=True)
+        prediction = grid(x).squeeze()
+        loss = torch.mean((prediction - observations)**2)
+        loss.backward()
+        optimiser.step()
+        optimiser.zero_grad()
+
+    _x = torch.linspace(0, 1, steps=100)
+    x = torch.meshgrid(_x, _x, indexing='xy')
+    x = einops.rearrange([*x], 'xy h w -> (h w) xy')
+    ground_truth = f(x)
+    prediction = grid(x).squeeze()
+    mean_absolute_error = torch.mean(torch.abs(prediction - ground_truth))
+    assert mean_absolute_error.item() < 0.02
+
+
+def test_3d_grid_optimisation():
+    grid_resolution = (6, 6, 6)
+    n_observations_per_iteration = 1000
+    grid = CubicBSplineGrid3d(resolution=grid_resolution, n_channels=1)
+
+    def f(x: torch.Tensor, add_noise: bool = False):
+        centered = x - 0.5
+        y = torch.sqrt(torch.sum(centered**2, dim=-1))  # (x**2 + y**2 + z**2) ** 0.5
+        if add_noise is True:
+            y += torch.normal(mean=torch.zeros(len(y)), std=0.3)
+        return y
+
+    optimiser = torch.optim.SGD(lr=1, params=grid.parameters())
+    for i in range(1000):
+        x = torch.rand(size=(n_observations_per_iteration, 3))
+        observations = f(x, add_noise=True)
+        prediction = grid(x).squeeze()
+        loss = torch.mean((prediction - observations)**2)
+        loss.backward()
+        optimiser.step()
+        optimiser.zero_grad()
+
+    _x = torch.linspace(0, 1, steps=100)
+    x = torch.meshgrid(_x, _x, _x, indexing='xy')
+    x = einops.rearrange([*x], 'xyz d h w -> (d h w) xyz')
     ground_truth = f(x)
     prediction = grid(x).squeeze()
     mean_absolute_error = torch.mean(torch.abs(prediction - ground_truth))